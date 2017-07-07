c6058c0e0d747d39e8fe11fa3303e564e75f3f07
bariflor <barihuamachuco@gmail.com>
2017-07-07 12:01:59 -0500

    modifique

--- ejer.js
+++ ejer.js
@@ -13,3 +13,20 @@ function crearMatriz(n){
   console.log (M);
 }
 crearMatriz(5);
+
+
+function crearMatriz(n){
+  var M = [];
+  var n = 5;
+  for(var i = 0; i < n; i++){
+    M[i] = [];
+    for(var j = 0; j < n; j++){
+      M[i][j]=0;
+      M[i][n-i-1]=5;
+      
+    }
+   
+  }
+  console.log (M);
+}
+crearMatriz(5);
